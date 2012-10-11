ca94b4756075e594dae0b4e808861342cd7908e2
anupong-s <anupong.s@carpass.co.th>
2012-10-11 14:42:48 +0700

    - Refactor code.
    Signed-off-by: anupong-s <anupong.s@carpass.co.th>

--- TransactionCommon/Encryption.cs
+++ TransactionCommon/Encryption.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Security.Cryptography;
 using System.IO;
 
-namespace TransactionCommon
+namespace CarPass.Transaction.Common
 {
 
     ///////////////////////////////////////////////////////////////////////////////
